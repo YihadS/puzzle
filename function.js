ac5b7a3ef24e86eef5221667dafcc2db26c761c6
Yihad Salek Villalba <72087335+YihadS@users.noreply.github.com>
2022-04-25 21:30:01 -0400

    Update function.js

--- function.js
+++ function.js
@@ -52,6 +52,8 @@ var c24 = document.getElementById('c24');
 var c25 = document.getElementById('c25');
 /*Final de elementos y inicio del drag & drop*/
 
+var cancion1 = document.getElementById('cancion1');
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -485,3 +487,19 @@ Swal.fire({
 }
 }
 },1);
+
+function reproducir(){
+  var reproducir1 = document.getElementById('reproducir1');
+  var reproducir2 = document.getElementById('reproducir2');
+  cancion1.play();
+  reproducir1.style.display="none";
+  reproducir2.style.display="block";
+}
+
+function mute(){
+  var reproducir1 = document.getElementById('reproducir1');
+  var reproducir2 = document.getElementById('reproducir2');
+  cancion1.pause();
+  reproducir2.style.display="none";
+  reproducir1.style.display="block";
+}
